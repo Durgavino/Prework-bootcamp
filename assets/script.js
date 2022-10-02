d38cbc260037f54356e23edee25b595b0118cab1
Durgavino <mailtodurga93@gmail.com>
2022-10-02 16:01:00 -0300

    Do not touch eol

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+var topic= ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topic.length; x++) 
+{
+    console.log(topic[x]);
+   }
+/*
+if(topic==='HTML')
+{
+    console.log("Let's study HTML!");
+}
+else if (topic ==='css')
+{
+    console.log("Let's study CSS!");
+
+}
+else if(topic==='GIT')
+{
+console.log("Let's study Git!");
+}
+else if(topic==='Javascript')
+{
+console.log("Let's study Javascript!");
+}
+else
+{
+    console.log('Please try again!');
+}
+*/
